c6700257d62c7ea775ba4931461f11c18bc9ecf3
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-28 14:39:14 +0300

    Filtering meals by categories

--- lib/category_detail_screen.dart
+++ lib/category_detail_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:course_007/dummy_data.dart';
 
 import 'models/category.model.dart';
+import 'models/meal.model.dart';
 
 class CategoryDetailsScreen extends StatelessWidget {
   static const routeName = '/category-detail';
 
   @override
   Widget build(BuildContext context) {
-    final category = ModalRoute.of(context).settings.arguments as Category;
+    final Category category =
+        ModalRoute.of(context).settings.arguments as Category;
+
+    final List<Meal> categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -16,9 +23,9 @@ class CategoryDetailsScreen extends StatelessWidget {
       ),
       body: Center(
         child: ListView.builder(
-          itemCount: 0,
+          itemCount: categoryMeals.length,
           itemBuilder: (context, index) {
-            return Text('');
+            return Text(categoryMeals[index].title);
           },
         ),
       ),
