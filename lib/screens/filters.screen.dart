5bdcce2e8c6c5ef8ddec771f1e02a0d4ce7e26a3
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-30 13:46:13 +0300

    Filter page

--- lib/screens/filters.screen.dart
+++ lib/screens/filters.screen.dart
@@ -1,8 +1,32 @@
 import 'package:course_007/widgets/index.dart';
 import 'package:flutter/material.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  Widget buildSwitch({
+    String title,
+    String subtitle,
+    bool value,
+    Function handler,
+  }) {
+    return SwitchListTile(
+      title: Text(title),
+      value: value,
+      subtitle: Text(subtitle),
+      onChanged: (change) => handler(change),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +35,56 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Filters'),
       ),
       drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                buildSwitch(
+                  title: 'Gluten Free',
+                  subtitle: 'Include gluten free meals',
+                  value: _glutenFree,
+                  handler: (change) {
+                    setState(() {
+                      _glutenFree = change;
+                    });
+                  },
+                ),
+                buildSwitch(
+                  title: 'Lactose Free',
+                  subtitle: 'Include lactose free meals',
+                  value: _lactoseFree,
+                  handler: (change) {
+                    setState(() {
+                      _lactoseFree = change;
+                    });
+                  },
+                ),
+                buildSwitch(
+                  title: 'Vegetarian',
+                  subtitle: 'Vegetarian meals',
+                  value: _vegetarian,
+                  handler: (change) {
+                    setState(() {
+                      _vegetarian = change;
+                    });
+                  },
+                ),
+                buildSwitch(
+                  title: 'Vegan',
+                  subtitle: 'Vegan meals',
+                  value: _vegan,
+                  handler: (change) {
+                    setState(() {
+                      _vegan = change;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
